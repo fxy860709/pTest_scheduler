fe81cf12b5508a8e7f58550363b5819c1eade1a9
fvafuc@126.com <fvafuc@126.com>
2023-01-10 11:15:25 +0800

    init

--- pTest_scheduler/views/penv.py
+++ pTest_scheduler/views/penv.py
@@ -1,6 +1,43 @@
 from flask import Blueprint
 from flask import Flask,render_template,request,redirect,url_for
 
+penv = Blueprint(
+    'env_manage',
+    __name__,
+    url_prefix=''
+)
+
+@penv.route('/env_manage')
+def env_manage_index():
+    env_infos = [
+        {
+            "id": 1,
+            "env_id": 123,
+            "env_ip": "1.1.1.1",
+            "env_cmc_dir": "/home/tools/cmc",
+            "env_manual": "V2环境"
+
+        },
+        {
+            "id": 1,
+            "env_id": 123,
+            "env_ip": "1.1.1.1",
+            "env_cmc_dir": "/home/tools/cmc",
+            "env_manual": "V3环境"
+        },
+        {
+            "id": 1,
+            "env_id": 123,
+            "env_ip": "1.1.1.1",
+            "env_cmc_dir": "/home/tools/cmc",
+            "env_manual": "V4环境"
+
+        }
+    ]
+    return render_template('pages/env_manage.html', env_infos=env_infos)
+
+    # return render_template('pages/env_manage.html', segment='index', navigation_name="环境管理", env_infos='env_infos')
+
 # penv = Blueprint('penv',__name__)
 #
 # @penv.route('/list')
@@ -10,15 +47,3 @@ from flask import Flask,render_template,request,redirect,url_for
 # @penv.route('/add')
 # def penv_add():
 #     return 'penvadd'
-
-penv = Blueprint(
-    'env_manage',
-    __name__,
-    url_prefix=''
-)
-
-@penv.route('/list')
-def env_manage_index():
-    # return 'penvlist'
-    # return render_template('account/login.html')
-    return render_template('accounts/env_manage.html', segment='index', navigation_name="环境管理", env_infos='env_infos')
